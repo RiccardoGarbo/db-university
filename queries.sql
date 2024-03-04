2ed3094c62986e96a591f4bd392c1da1b5d732ed
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-04 17:01:11 +0100

    query select join 4

--- queries.sql
+++ queries.sql
@@ -93,3 +93,12 @@ FROM
     JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`
 WHERE
     `teachers`.`name` = 'Fulvio' && `teachers`.`surname` = 'Amato';
+
+--4 Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`name` as 'Nome studente', `students`.`surname` as 'Cognome studente', `degrees`.*, `departments`.`name` AS 'Nome dipartimento'
+FROM
+    `students`
+    JOIN `degrees` ON `degrees`.`id` = `students`.`degree_id`
+    JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname` ASC, `students`.`name` ASC;
