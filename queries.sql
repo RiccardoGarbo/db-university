04d33cd14819e372216fd4c24402c63c75be9956
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-04 16:24:44 +0100

    query select join 1

--- queries.sql
+++ queries.sql
@@ -6,9 +6,12 @@ SELECT * FROM `students` WHERE YEAR(date_of_birth) = 1990;
 
 SELECT * FROM `courses` WHERE cfu > 10;
 
---3 Selezionare tutti gli studenti che hanno più di 30 anni 
+--3 Selezionare tutti gli studenti che hanno più di 30 anni
 
-SELECT * FROM `students` WHERE DATEDIFF(curdate(), date_of_birth) > 30;
+SELECT *
+FROM `students`
+WHERE
+    DATEDIFF(curdate(), date_of_birth) > 30;
 
 --4 Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
@@ -16,7 +19,11 @@ SELECT * FROM `courses` where period = 'I semestre' AND year = 1;
 
 --5 Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-SELECT * FROM `exams` WHERE DATE(date) = '2020-06-20' AND hour > '14:00:00';
+SELECT *
+FROM `exams`
+WHERE
+    DATE(date) = '2020-06-20'
+    AND hour > '14:00:00';
 
 --6 Selezionare tutti i corsi di laurea magistrale (38)
 
@@ -24,29 +31,48 @@ SELECT * FROM `degrees` WHERE name LIKE '%laurea magistrale%';
 
 --7 Da quanti dipartimenti è composta l'università? (12)
 
-SELECT count(*)  FROM `departments`
+SELECT count(*)
+FROM `departments`
+    --8 Quanti sono gli insegnanti che non hanno un numero di telefono ? (50)
 
---8 Quanti sono gli insegnanti che non hanno un numero di telefono ? (50)
-
-SELECT count(*) FROM `teachers` where phone is NOT null;
+SELECT count(*) FROM `teachers` where phone is null;
 
 -- 1group Contare quanti iscritti ci sono stati ogni anno
 
-SELECT count(*) as studenti_iscritti , year(enrolment_date) FROM `students` GROUP by YEAR(enrolment_date);
+SELECT count(*) as studenti_iscritti, year(enrolment_date)
+FROM `students`
+GROUP by
+    YEAR(enrolment_date);
 
 --2group  Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
-SELECT count(*) as number_of_teachers , office_address FROM `teachers` GROUP BY office_address;
+SELECT
+    count(*) as number_of_teachers,
+    office_address
+FROM `teachers`
+GROUP BY
+    office_address;
 
 --3group Calcolare la media dei voti di ogni appello d'esame
 
-SELECT ROUND(AVG(vote),2) as media_voto , exam_id as numero_appello FROM `exam_student` GROUP by numero_appello;
+SELECT
+    ROUND(AVG(vote), 2) as media_voto,
+    exam_id as numero_appello
+FROM `exam_student`
+GROUP by
+    numero_appello;
 
---4 Contare quanti corsi di laurea ci sono per ogni dipartimento
+--4group Contare quanti corsi di laurea ci sono per ogni dipartimento
 
-SELECT count(name) as numero_corsi , department_id FROM `degrees` GROUP by department_id;
+SELECT count(name) as numero_corsi, department_id
+FROM `degrees`
+GROUP by
+    department_id;
 
+--1join Selezionare tutti gli studenti al Corso di Laurea in Economia
 
-
- 
-
+SELECT *
+FROM `students`
+    JOIN `degrees` ON `degrees`.`id` = `students`.`degree_id`
+WHERE
+    `degrees`.`name` = 'Corso di Laurea in Economia';
